260a829c03a21174b236211f457ab113ddc5b9e7
Antoine Nauleau <ajnauleau@gmail.com>
2019-11-26 15:18:56 +0100

    WIP events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_secure_token
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :token, presence: true
 end
