b365a64b6f82c569f94d1ee80dbdfb652fbda225
Raphaële Four <raphaelefour@gmail.com>
2019-11-27 18:41:26 +0100

    ajout-idee-ok

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,21 +1,29 @@
 class IdeasController < ApplicationController
 
-
   def index
     @ideas = Idea.all
   end
 
   def new
     @idea = Idea.new
+    @list = current_user.lists.find(params[:list_id])
   end
 
   def create
+    @list = current_user.lists.find(params[:list_id])
     @idea = Idea.new(ideas_params)
-    @idea.save
+    @idea.user = current_user
+    @idea.list = @list
+
+    if @idea.save
+      redirect_to event_path(@list.event)
+    else
+      render :new
+    end
   end
 
   def ideas_params
-    params.require(:idea).permit(:name, :date, :token)
+    params.require(:idea).permit(:title, :description, :url, :photo)
   end
 
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,7 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :user
   belongs_to :list
-  belongs_to :chosen_by, class_name: "User", foreign_key: "chosen_by_id"
+  belongs_to :chosen_by, optional: true, class_name: "User", foreign_key: "chosen_by_id"
   has_many :comments
   has_one_attached :photo
 end
