0c44f38721a68ead289a244622bb420fe0ba4760
bosc kevin <kevin_bosc@hotmail.fr>
2019-11-27 11:45:07 +0100

    changement

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,8 @@
 class EventsController < ApplicationController
-
   def index
     @events = Event.all
     @event = Event.new
   end
-
-
 
   def show
     @event = Event.find(params[:id])
@@ -23,6 +20,4 @@ class EventsController < ApplicationController
   def events_params
     params.require(:event).permit(:name, :date, :token)
   end
-
-
 end
