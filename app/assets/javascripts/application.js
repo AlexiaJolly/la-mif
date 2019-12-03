369114fea42a87c8608330945639a7bce04b02ac
Alexia Jolly <alexia.jolly@hotmail.com>
2019-12-03 18:04:22 +0100

    encours

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,3 @@
 //= require_tree .
 
 
-const comment = document.querySelector("comments-form")
-element.addEventListener(eventType, (event) => {
-  // Do something (callback)
-  console.log(event)
-});
