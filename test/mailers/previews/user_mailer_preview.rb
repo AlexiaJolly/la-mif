ba4d99867b61af1ed1d05b890ece87b305e34c94
bosc kevin <kevin_bosc@hotmail.fr>
2019-12-03 10:25:37 +0100

    back mailer fini

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,5 +1,5 @@
 class UserMailerPreview < ActionMailer::Preview
   def welcome
-    UserMailer.with(email: 'dev@gmail.com', inviter: User.first, event: Event.last).welcome
+    UserMailer.with(email: 'la.mif.wagon@gmail.com', inviter: User.first, event: Event.last).welcome
   end
 end
