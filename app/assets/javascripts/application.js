0cbf191f6edcecb1dea9ee6fe97530cd32d9f755
bosc kevin <kevin_bosc@hotmail.fr>
2019-12-03 18:51:25 +0100

    copy url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-
-const comment = document.querySelector("comments-form")
-element.addEventListener(eventType, (event) => {
-  // Do something (callback)
-  console.log(event)
-});
